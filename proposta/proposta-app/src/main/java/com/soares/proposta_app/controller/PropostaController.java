5400bcec787e4889483d8980a369c627f8cf35a9
joaov12 <jvfs12th@gmail.com>
2024-11-18 09:44:21 -0300

    build: Cria controller de Proposta

---
+++ proposta/proposta-app/src/main/java/com/soares/proposta_app/controller/PropostaController.java
@@ -0,0 +1,34 @@
+package com.soares.proposta_app.controller;
+
+import com.soares.proposta_app.dto.PropostaRequestDto;
+import com.soares.proposta_app.dto.PropostaResponseDto;
+import com.soares.proposta_app.service.PropostaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/proposta")
+public class PropostaController {
+
+    @Autowired
+    private PropostaService propostaService;
+
+    @PostMapping
+    public ResponseEntity<PropostaResponseDto> criar(@RequestBody PropostaRequestDto requestDto){
+        PropostaResponseDto response = propostaService.criar(requestDto);
+        return ResponseEntity.created(ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(response.getId())
+                .toUri())
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PropostaResponseDto>> obterProposta(){
+        return ResponseEntity.ok(propostaService.obterProposta());
+    }
+}
