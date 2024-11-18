5c960e0bb4aa724f471805f7ccc1997dcbdfd228
joaov12 <jvfs12th@gmail.com>
2024-11-18 09:41:10 -0300

    feat: Adiciona mapeamento dos DTOs com o MapStruct

---
+++ proposta/proposta-app/src/main/java/com/soares/proposta_app/mapper/PropostaMapper.java
@@ -0,0 +1,43 @@
+package com.soares.proposta_app.mapper;
+
+import com.soares.proposta_app.dto.PropostaRequestDto;
+import com.soares.proposta_app.dto.PropostaResponseDto;
+import com.soares.proposta_app.entity.Proposta;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import java.text.NumberFormat;
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface PropostaMapper {
+
+    PropostaMapper INSTANCE = Mappers.getMapper(PropostaMapper.class);
+
+    @Mapping(target = "usuario.nome", source = "nome")
+    @Mapping(target = "usuario.sobrenome", source = "sobrenome")
+    @Mapping(target = "usuario.cpf", source = "cpf")
+    @Mapping(target = "usuario.telefone", source = "telefone")
+    @Mapping(target = "usuario.renda", source = "renda")
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "aprovada", ignore = true)
+    @Mapping(target = "integrada", constant = "true")
+    @Mapping(target = "observacao", ignore = true)
+    Proposta convertDtoToProposta(PropostaRequestDto propostaRequestDto);
+
+    @Mapping(target = "nome", source = "usuario.nome")
+    @Mapping(target = "sobrenome", source = "usuario.sobrenome")
+    @Mapping(target = "telefone", source = "usuario.telefone")
+    @Mapping(target = "cpf", source = "usuario.cpf")
+    @Mapping(target = "renda", source = "usuario.renda")
+    @Mapping(target = "valorSolicitadoFmt", expression = "java(setValorSolicitadoFmt(proposta))")
+    PropostaResponseDto convertEntityToDto(Proposta proposta);
+
+
+    List<PropostaResponseDto> convertListEntityToListDto(Iterable<Proposta> proposta);
+
+    default String setValorSolicitadoFmt(Proposta proposta){
+        return NumberFormat.getCurrencyInstance().format(proposta.getValorSolicitado());
+    }
+}
