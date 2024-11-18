854fef0d91df0d9effb7aa650ff9b09d5a60a1eb
joaov12 <jvfs12th@gmail.com>
2024-11-18 09:37:34 -0300

    init: Inicia microsserviço principal de proposta

---
+++ proposta/proposta-app/src/test/java/com/soares/proposta_app/PropostaAppApplicationTests.java
@@ -0,0 +1,14 @@
+/*package com.soares.proposta_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropostaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
