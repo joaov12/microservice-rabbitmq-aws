8757028bccef84bd758f0c703f604b54d43f88af
joaov12 <jvfs12th@gmail.com>
2024-11-19 07:40:03 -0300

    feat: Adiciona modelo de Usuario

---
+++ notificacao/notificacao/src/main/java/com/soares/notificacao/domain/Usuario.java
@@ -0,0 +1,20 @@
+package com.soares.notificacao.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Usuario {
+    private Long id;
+
+    private String nome;
+
+    private String sobrenome;
+
+    private String cpf;
+
+    private String telefone;
+
+    private Double renda;
+}
