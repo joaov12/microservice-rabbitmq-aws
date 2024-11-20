2fb8ea259e4800fd5cd830c0153cf7ec496c80cb
joaov12 <jvfs12th@gmail.com>
2024-11-19 23:58:40 -0300

    feat: Adiciona mensagem constante para envio de notificações via Amazon SNS

---
+++ notificacao/notificacao/src/main/java/com/soares/notificacao/constant/MensagemConstante.java
@@ -0,0 +1,5 @@
+package com.soares.notificacao.constant;
+
+public class MensagemConstante {
+    public static final String PROPOSTA_EM_ANALISE = "Prezado(a) %s, sua proposta foi recebida por nossa equipe. Em breve retornamos o contato!";
+}
