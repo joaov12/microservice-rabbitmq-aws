ddb2ed35860b4c342d0add11772c90ad9221ef48
joaov12 <jvfs12th@gmail.com>
2024-11-20 00:00:05 -0300

    init: Inicia microservice para analises de créditos

---
+++ analisecredito/analisecredito/src/main/java/com/soares/analisecredito/AnalisecreditoApplication.java
@@ -0,0 +1,15 @@
+package com.soares.analisecredito;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalisecreditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalisecreditoApplication.class, args);
+	}
+
+
+}
