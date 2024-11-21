9f0d2f409b76f9b3a06062ae92476a4b7978d477
joaov12 <jvfs12th@gmail.com>
2024-11-21 09:40:54 -0300

    feat: Adiciona exceção personalizada para pagamento inferior a dez anos

---
+++ analisecredito/analisecredito/src/main/java/com/soares/analisecredito/service/strategy/impl/PrazoPagamentoInferiorDezAnos.java
@@ -0,0 +1,14 @@
+package com.soares.analisecredito.service.strategy.impl;
+
+import com.soares.analisecredito.domain.Proposta;
+import com.soares.analisecredito.service.strategy.CalculoPonto;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PrazoPagamentoInferiorDezAnos implements CalculoPonto {
+
+    @Override
+    public int calcular(Proposta proposta) {
+        return proposta.getPrazoPagamento() < 120 ? 80 : 0;
+    }
+}
